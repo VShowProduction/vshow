ed6191d2d300454b3f86b370d7f703ca9d02b561
Liroy34 <slchaves@cnv.gov.ar>
2024-08-22 10:51:16 -0300

    Agegado principio de footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer-container">
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+        <div>
+            <h5>© {{ Year }} VSHOW PROJECT | TODOS LOS DERECHOS RESERVADOS</h5>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const date = Date.now();
+const Year = ref(date.Year)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
