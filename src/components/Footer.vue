67af2765693edaf090837078c369cdd7bcd5fd06
Liroy34 <slchaves@cnv.gov.ar>
2024-08-22 14:08:02 -0300

    Agregado ICONOS y FontAwesome al Proyecto

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="footer-container">
         <div>
-
+            <ul>
+                <li><a href="https://x.com/VshowProduction" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" /></a></li>
+                <li><a href="https://www.twitch.tv/tecnoanime" target="_blank"><font-awesome-icon :icon="['fab', 'twitch']" /></a></li>
+            </ul>
         </div>
         <div>
 
         </div>
 
         <div>
-            <h5>© {{ Year }} VSHOW PROJECT | TODOS LOS DERECHOS RESERVADOS</h5>
+            <h3>© {{ Year }} VSHOW PROJECT | TODOS LOS DERECHOS RESERVADOS</h3>
         </div>
     </div>
 </template>
@@ -16,8 +19,10 @@
 <script setup>
 import { ref } from 'vue';
 
-const date = Date.now();
-const Year = ref(date.Year)
+const date = new Date;
+const Year = ref(date.getFullYear())
+console.log(date)
+console.log(Year)
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import './css/main.scss'
 import 'animate.css';
 
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter,  faTwitch } from '@fortawesome/free-brands-svg-icons';
 
+
+
+library.add (faTwitch, faTwitter, faUserSecret)
 
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.mount('#app');
 
