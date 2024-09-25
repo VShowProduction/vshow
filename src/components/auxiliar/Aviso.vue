396af614fdcb3d0c76c5fe1351e3523f962e6b58
Santiago Lujan Chaves <Liroy34>
2024-09-25 15:47:09 -0300

    modifcado avisos

--- src/components/auxiliar/Aviso.vue
+++ src/components/auxiliar/Aviso.vue
@@ -1,13 +1,17 @@
 <template>
     <Transition name="slide-fade">
+
       <div v-if="visible" class="aviso">
         <button class="close-btn" @click="closeAviso">×</button> <!-- Botón de cierre -->
+        <RouterLink to="/FormularioWorldCup">
         <div class="aviso-content">
           <img src="/LOGO_WC.png" width="70px"/>
           <p>YA ESTA DISPONIBLE LA INSCRIPCION PARA LA <span>VT WORLD CUP 2</span></p>
-          <RouterLink to="/FormularioWorldCup">Ir al formulario</RouterLink>
+          <span>Ir al formulario</span>
         </div>
+         </RouterLink>
       </div>
+
     </Transition>
   </template>
   
